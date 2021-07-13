f49d44a347b9dc0694f286945bc9b24e0d988119
Gyumong <rlaalsrb3559@naver.com>
2021-07-13 18:05:18 +0900

    🎨 모바일 사이드 메뉴 구현

--- src/components/MobileMenu/index.js
+++ src/components/MobileMenu/index.js
@@ -1,12 +1,82 @@
 /** @format */
 
 import React from "react";
-import { MobileWrapper, MobileBlock } from "./styles";
-
+import {
+  MobileWrapper,
+  MobileBlock,
+  TitleTop,
+  ProfileBlock,
+  TopBar,
+  TopItem,
+} from "./styles";
+import { CloseOutlined } from "@ant-design/icons";
 const MobileMenu = () => {
   return (
     <MobileWrapper>
-      <MobileBlock>MobileMenu</MobileBlock>
+      <MobileBlock>
+        <TitleTop>
+          <i>로고</i>
+          <button>
+            <CloseOutlined style={{ fontSize: "24px" }} />
+          </button>
+        </TitleTop>
+        <ProfileBlock>
+          <TopBar>
+            <TopItem>
+              <span>My 원티드</span>
+              <div>아이콘</div>
+            </TopItem>
+            <TopItem>
+              <div>프로필</div>
+            </TopItem>
+            <TopItem
+              style={{
+                paddingTop: "15px",
+                borderTop: "1px solid #eee",
+                marginTop: "15px",
+              }}
+            >
+              <div style={{ padding: "15px 0" }}>이력서</div>
+            </TopItem>
+            <TopItem>
+              <div>매치업</div>
+            </TopItem>
+            <TopItem>
+              <div>추천</div>
+            </TopItem>
+            <TopItem>
+              <div>지원 현황</div>
+            </TopItem>
+            <TopItem>
+              <div>프리랜서</div>
+            </TopItem>
+            <TopItem
+              style={{
+                paddingTop: "15px",
+                borderTop: "1px solid #eee",
+                marginTop: "15px",
+              }}
+            >
+              <div style={{ padding: "15px 0" }}>직군별 연봉</div>
+            </TopItem>
+            <TopItem>
+              <div>커리어 성장</div>
+            </TopItem>
+            <TopItem
+              style={{
+                paddingTop: "15px",
+                borderTop: "1px solid #eee",
+                marginTop: "15px",
+              }}
+            >
+              <div style={{ padding: "15px 0" }}>기업 서비스</div>
+            </TopItem>
+            <TopItem>
+              <div style={{ color: "#999999" }}>로그아웃</div>
+            </TopItem>
+          </TopBar>
+        </ProfileBlock>
+      </MobileBlock>
     </MobileWrapper>
   );
 };
--- src/components/MobileMenu/styles.js
+++ src/components/MobileMenu/styles.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 export const MobileWrapper = styled.div`
   @media (max-width: 767px) {
+    display: flex;
     position: fixed;
     top: 0;
     right: 0;
@@ -12,6 +13,7 @@ export const MobileWrapper = styled.div`
     margin-top: 0;
     -webkit-transform: none;
     transform: none;
+    background-color: white;
   }
 `;
 export const MobileBlock = styled.div`
@@ -21,9 +23,53 @@ export const MobileBlock = styled.div`
     border-radius: 0;
     z-index: 1;
     width: 100%;
-    height: 100%;
+    height: 100vh;
     overflow-x: hidden;
     overflow-y: auto;
     padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
+`;
+
+export const TitleTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 50px;
+  i {
+    display: inline-block;
+    font-size: 24px;
+    line-height: 1;
+    padding: 0;
+    margin: 13px 0;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
+
+export const ProfileBlock = styled.div`
+  margin: 45px 0 15px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const TopBar = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+`;
+
+export const TopItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: auto;
+  padding: 15px 0;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 1;
 `;
